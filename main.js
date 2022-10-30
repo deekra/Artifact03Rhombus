dec28e1741c15d71552f37d48dd937e68c606514
deekra <114116356+deekra@users.noreply.github.com>
2022-10-29 22:22:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -65,6 +65,36 @@ rLine +="</p>";
 document.getElementById("upRight").innerHTML = rLine;
 }
 
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+// Fill in the left side of each row with spaces or blanks
+for (x=1; x<=pHeight-i;x++){
+  //rLine +="-";
+  rLine +="<span class='space'>" + pSymbol +"</span>";
+}
+  
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = rLine;
+}
+
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
